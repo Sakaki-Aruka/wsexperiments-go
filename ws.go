e5288ba63b17926ece1599e0dcde14737e1326d7
Sakaki-Aruka <aruka.works@gmail.com>
2025-09-26 03:15:46 +0900

    fix: websocket client input

--- ws.go
+++ ws.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/websocket"
 )
@@ -14,34 +17,91 @@ import (
 var u = websocket.Upgrader{}
 
 func main() {
+	flag.Parse()
 	args := flag.Args()
 	if len(args) == 0 {
 		server()
 	} else {
-		client(args)
+		fmt.Println("client")
+		client()
 	}
 }
 
-func client(args []string) {
-	Url := url.URL{Scheme: "ws", Host: ":8888", Path: "/socket"}
-	conn, _, err := websocket.DefaultDialer.Dial(Url.String(), nil)
-	if err != nil {
-		fmt.Println("client connect error: " + err.Error())
-		return
-	}
+func client() {
+	u := url.URL{Scheme: "ws", Host: "localhost:8888", Path: "/socket"}
 
-	if err := conn.WriteMessage(1, []byte(strings.Join(args, " "))); err != nil {
-		fmt.Println("websocket write error: " + err.Error())
-		return
-	}
-	t, d, err := conn.ReadMessage()
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		fmt.Println("websocket response error: " + err.Error())
-		return
+		log.Fatal("dial:", err)
 	}
+	defer c.Close()
 
-	fmt.Println(fmt.Sprintf("type: %v, content (Received): %v", t, string(d)))
-	conn.Close()
+	done := make(chan struct{}) // 読み取りゴルーチンの終了を通知
+	stop := make(chan struct{}) // 書き込みゴルーチンに終了を指示
+
+	// サーバーからのメッセージを読み取るゴルーチン
+	go func() {
+		defer close(done) // 終了時にdoneチャネルを閉じる
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	// 標準入力からユーザーの入力を処理するゴルーチン
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			// stopチャネルからのシグナルを待つ
+			select {
+			case <-stop:
+				log.Println("stdin goroutine stopping...")
+				return
+			default:
+				// ブロックしないようにデフォルトケースを使用
+			}
+
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				log.Println("stdin read error:", err)
+				return
+			}
+
+			err = c.WriteMessage(websocket.TextMessage, []byte(line))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		}
+	}()
+
+	// 正常なシャットダウン処理
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-done:
+			// サーバーからの読み取りゴルーチンが終了した場合
+			log.Println("Read goroutine finished.")
+			return
+		case <-interrupt:
+			// シャットダウンシグナルを受信した場合
+			log.Println("interrupt")
+			close(stop) // 書き込みゴルーチンに停止を指示
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+			}
+			// サーバーからの読み取りゴルーチンが終了するのを待つ
+			<-done
+			return
+		}
+	}
 }
 
 func h(w http.ResponseWriter, r *http.Request) {
